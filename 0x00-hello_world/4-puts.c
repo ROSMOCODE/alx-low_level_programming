c778a8039259b854d6777c095752ba6e6cee4a1a
rosmocode <rosmoatancha@gmail.com>
2023-02-26 14:51:35 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
-* main - main entry point
-* return : always 0 success
+*main -Entry point
+*Description: 'the program's 
+*  Return: Always 0 (Success)
 */
 int main(void)
 {
