720e41ff50404fa526e574748cf30b048561bc50
rosmocode <rosmoatancha@gmail.com>
2023-02-26 14:43:53 -0800

    inputs

--- 0x00-hello_world/4-inputs.c
+++ 0x00-hello_world/4-inputs.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
-* main - main entry point
-* return : always 0 success
+*  main -Entry point
+*
+*  Return: Always 0 (Success)
 */
 int main(void)
 {
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+* main - entry point
+* return: return 0 always for success
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
