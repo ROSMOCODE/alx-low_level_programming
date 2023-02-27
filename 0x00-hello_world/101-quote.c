fc0702775932a53e056453379771aedcc3690673
rosmocode <rosmoatancha@gmail.com>
2023-02-27 03:51:51 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,13 @@
-/**
-*File: 101-quote.c
-*Auth: Brennan D Baraban
-*/
-
 #include <unistd.h>
-
-/*
-*main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
-*followed by a new line, to standard error.
-*description
-*Return: Always 1.
+/**
+* main - Entry point
+*
+* Return: Always 1 (Success)
 */
 int main(void)
 {
-	write(2,
-	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	59);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
+	write(2, str1, 59);
 	return (1);
 }
